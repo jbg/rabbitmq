f5eb9531a0670ba8022ad26bf080ac5adbb5d1d0
Jasper Bryant-Greene <jbg@rf.net.nz>
2015-11-29 14:38:04 +0100

    Added example of publish to example.py

--- example.py
+++ example.py
@@ -19,6 +19,12 @@ conn.declare_queue(1, "queuename")
 conn.bind_queue(1, "queuename", "queuename", "queuename")
 conn.consume(1, "queuename")
 
+publish_conn = rabbit.Connection()
+publish_conn.open(AMQP_BROKER_HOST, AMQP_BROKER_PORT)
+publish_conn.login(AMQP_BROKER_VHOST, 0, AMQP_BROKER_USER, AMQP_BROKER_PASSWORD)
+publish_conn.open_channel(1)
+publish_conn.declare_exchange(1, "results", "direct")
+
 while True:
     envelope = conn.consume_message()
     if envelope.message.properties.content_type == "application/x-msgpack":
@@ -26,7 +32,18 @@ while True:
         decoded_body = msgpack.unpackb(envelope.message.body, encoding='utf-8', use_list=False)
         gc.enable()
         conn.ack(1, envelope.delivery_tag)
+
         # do something with decoded_body
+
+        # publish something, maybe result of operation
+        props = rabbit.Properties(content_type="application/x-msgpack", delivery_mode=1)
+        body = msgpack.packb(payload)
+        if len(body) > 8192:
+            # example of compressing large message bodies
+            from bz2 import compress
+            body = compress(body)
+            props.content_encoding = "bzip2"
+        publish_conn.publish(1, "results", envelope.message.properties.reply_to, body, props)
     else:
         log.warning("Unexpected content-type: %s", envelope.message.properties.content_type)
 
